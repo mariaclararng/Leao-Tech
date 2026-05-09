598b368234375d47d574cec8ab3dbba73cfc2cf8
Maria Clara da Silva <12508272485@espcid.intra>
2026-05-08 21:48:54 -0300

    Aprendendo, Adaptando e Melhorando: React e JS

---
+++ Aula-React/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Home from './pages/Home/Home'
+
+
+function App() {
+  return (<div>
+
+    <Home/>
+    <Footer/>
+    <Header/>
+  </div>
+  )
+}
+
+export default App
---
+++ Aula-React/src/pages/Home/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <header>uirhgoriruitophtopnttkititi</header>
+  )
+}
+
+export default Home
